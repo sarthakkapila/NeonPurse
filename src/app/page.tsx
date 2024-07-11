55d665290706738756404aa8d79db2eaa410a85f
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-07-11 18:35:58 +0530

    page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,39 @@
+
 import { GridBackgroundDemo } from "@/components/grid";
 import { Navbar } from "@/components/navbar-landing";
+import { SpotlightPreview } from "@/components/gridspot";
+import { NavbarDemo } from "@/components/navbar";
+
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-grid bg-gray-950">
-      <div className="flex justify-between p-4">
-        <div className="text-3xl font-extrabold">
+    <div className="min-h-screen bg-zinc-950 bg-grid p-4 ">
+      <div className="flex justify-between items-center">
+        <div className="text-4xl sm:text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-950 py-4">
           NeonPurse
         </div>
-        <div>
-          <Navbar className="p-8" />
+        <div className="p-10">
+          <NavbarDemo />
         </div>
         <div>
-          <button className="p-[3px] relative flex justify-end">
-            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-950 rounded-lg" />
-            <div className="px-8 py-4 relative group transition duration-200 text-white font-extrabold text-xl hover:bg-transparent">
+
+          <button className="relative p-[3px] flex justify-end">
+            <div className="absolute inset-0 bg-gradient-to-r from-zinc-400 to-gray-950 rounded-lg" />
+            <div className="relative px-8 py-4 text-white font-extrabold text-xl bg-transparent hover:bg-transparent transition duration-200">
               Launch App
             </div>
           </button>
         </div>
       </div>
-      <GridBackgroundDemo />
+      <div className="max-w-screen-2xl mx-auto px-2 py-4 pt-16 ">
+        <SpotlightPreview />
+
+
+        <div className="bg-stone-700 rounded-xl">
+          <h1 className="text-lg font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-950 py-2">Earn points by using our wallets</h1>
+        </div>
+        <GridBackgroundDemo />
+      </div>
     </div>
   );
 }
-
