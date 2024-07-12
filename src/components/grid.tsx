8d30a7ffc5d273fcf0937629bfcbfdd4cc70d481
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-07-13 02:23:32 +0530

    Added backgrounds

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,78 +1,35 @@
 
 import React from "react";
 import Image from 'next/image'
-import eth from '../../public/eth.png';
-import sol from '../../public/sol.png';
-import leverage from '../../public/leverage.png';
-import people from '../../public/people.png';
+import p1 from '../../public/p1.png';
+import p2 from '../../public/p2.png';
 
-export function GridBackgroundDemo() {
+export function GridBackground() {
   return (
-    <div className="h-auto w-full bg-black bg-grid-white/[0.1] relative flex flex-col items-center justify-center py-20">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <div className="pt-30 text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2">
-        Our Ecosystem
-      </div>
+    <div className="h-auto w-full bg-black bg-grid-white/[0.1] relative flex flex-col items-center justify-center py-20 rounded-full">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  rounded-full [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex flex-col sm:flex-row justify-center items-center sm:space-x-8 mt-8 relative z-20">
         <div className="flex flex-col items-center p-8">
           <Image
-            src={eth}
+            src={p1}
             alt="Ethereum"
-            width={200}
-            height={200}
+            width={500}
+            height={500}
           />
-          <p className="text-xl p-30 text-white mt-2">Ethereum</p>
+          <p className="text-xl p-30 text-white font-medium mt-2">Crypto to fiat off-ramp</p>
         </div>
         <div className="flex flex-col items-center p-8">
           <Image
-            src={sol}
+            src={p2}
             alt="Solana"
-            width={200}
-            height={200}
+            width={500}
+            height={500}
           />
-          <p className="text-xl p-30 text-white mt-2">Solana</p>
+          <p className="text-xl p-30 text-white font-medium mt-2">Fiat to crypto on-ramp</p>
         </div>
       </div>
-      <div className="pb-20 text-5xl pb-30 font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2">
-        All in one wallet
-      </div>
-      <div className="bg-stone-500 rounded-xl w-full">
-        <h1 className="text-lg font-extrabold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-400 py-2">
-          go-to solution for all your DeFi needs
-        </h1>
-      </div>
-      <div className="flex flex-col sm:flex-row justify-between items-center w-full sm:space-x-8 mt-8 relative z-20 px-4">
-        <div className="flex flex-col items-center p-8">
-          <div className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
-            Decentralized, Secure & Reliable
-          </div>
-          <div className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 mt-4">
-            Become part of a decentralized venture capital and development community with Metavault DAO!
-          </div>
-        </div>
-        <div className="p-8">
-          <Image
-            src={leverage}
-            alt="Leverage"
-            width={700}
-            height={400}
-          />
-        </div>
-      </div>
-      <div className="flex flex-col sm:flex-row justify-between items-center w-full sm:space-x-8 mt-8 relative z-20 px-4">
-        <div className="flex flex-col items-center p-8">
-          <div className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
-            Trade top Markets with up to 100x leverage directly from your Web3 wallet
-          </div>
-        </div>
-        <div className="p-8">
-          <Image
-            src={people}
-            alt="People"
-            width={700}
-            height={400}
-          />
-        </div>
+      <div className="text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-2">
+        Based on Pancake-V3
       </div>
     </div >
   );
--- src/components/gridspot.tsx
+++ src/components/gridspot.tsx
@@ -1,29 +1,61 @@
 
+"use client"
 import React from "react";
-import { cn } from "@/utils/cn";
-import { Spotlight } from "./ui/Spotlight.tsx";
-
+import { Spotlight } from "@/components/ui/Spotlight";
+import TypingAnimation from "@/components/TypingAnimation";
+import { motion } from "framer-motion";
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
 export function SpotlightPreview() {
   return (
-    <div className="h-[60rem] w-full rounded-3xl flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[60rem] w-full rounded-lg flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="gray"
+        fill="white"
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Neon Purse is all you need.
+        <h1 className="text-4xl md:text-7xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Neon Purse
+          <br />
+          <motion.h1 variants={textVariants} className="text-4xl md:text-7xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+            <TypingAnimation />
+          </motion.h1>
         </h1>
-        <div className="flex justify-center space-x-20 p-14">
+        <p className="mt-3 bg-clip-text text-left font-bold text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 sm:mt-5 sm:text-lg smrmax-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          NeonPurse is a decentralized, secure, and easy-to-use wallet.
+          <br className="p-10" />
+          Swap, stake, and earn with a splash of neon!
+        </p>
+        <div className="flex justify-start space-x-8 pt-5">
           <button className="relative p-[3px] flex justify-end">
             <div className="absolute inset-0 bg-gradient-to-r from-neutral-600 to-neutral-800 rounded-lg" />
-            <div className="relative px-8 py-4 p-20 text-gray-300 font-extrabold text-xl bg-transparent hover:bg-transparent transition duration-200">
+            <div className="relative px-8 py-4 text-gray-300 font-extrabold text-xl bg-transparent hover:bg-transparent transition duration-200">
               Create Wallet
             </div>
           </button>
           <button className="relative p-[3px] flex justify-end">
             <div className="absolute inset-0 bg-gradient-to-r from-neutral-600 to-neutral-800 rounded-lg" />
-            <div className="relative px-8 py-4 p-20 text-gray-300 font-extrabold text-xl bg-transparent hover:bg-transparent transition duration-200">
+            <div className="relative px-8 py-4 text-gray-300 font-extrabold text-xl bg-transparent hover:bg-transparent transition duration-200">
               Import Wallet
             </div>
           </button>
