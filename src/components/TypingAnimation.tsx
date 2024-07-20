4fc30ddd623af66a58680a13726400b829450b64
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-07-20 19:01:45 +0530

    fix

--- src/components/TypingAnimation.tsx
+++ src/components/TypingAnimation.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
 const TypingAnimation = () => {
-  return <TypeAnimation sequence={["all you need.", 1000, "lightning fast.", 1000, "has bank grade security.", 1000, "everyone's favourite Dex.", 1000]} wrapper="span" speed={50} className="text-4xl md:text-7xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50" repeat={Infinity} />;
+  return <TypeAnimation sequence={["is all you need.", 1000, "is lightning fast.", 1000, "has bank grade security.", 1000, "is everyone's favourite Dex.", 1000]} wrapper="span" speed={50} className="text-4xl md:text-7xl font-bold text-left bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50" repeat={Infinity} />;
 };
 
 export default TypingAnimation;
