6c0683508a77d494bcf630c3cbfb1d62a100cf88
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-07-20 18:13:52 +0530

    navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 
-export function NavbarDemo() {
+export function AppNavbar() {
   return (
     <div className="relative w-full flex items-center justify-center">
       <Navbar className="top-2" />
@@ -17,10 +17,6 @@ function Navbar({ className }: { className?: string }) {
   return (
     <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 text-zinc-950 text-xl font-semibold p-6", className)}>
       <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Swap">
-          <div className="flex flex-col space-y-4 text-sm">
-          </div>
-        </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Wallet">
           <div className="  text-sm grid grid-cols-2 gap-10 p-4">
           </div>
