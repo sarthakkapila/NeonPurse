3f026cc127c4c2b00db39cad21f4d18d133de5cc
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-07-20 18:00:26 +0530

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,9 @@ export function SiteFooter() {
     <footer>
       <div className="container flex items-center justify-center md:h-24 md:flex-row">
         <p className=" text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{" "}
+          Made by{" "}
           <a
             href={siteConfig.links.twitter}
-            target="_blank"
             rel="noreferrer"
             className="font-medium underline"
           >
@@ -17,7 +16,6 @@ export function SiteFooter() {
           . The source code is available on{" "}
           <a
             href={siteConfig.links.github}
-            target="_blank"
             rel="noreferrer"
             className="font-medium underline"
           >
